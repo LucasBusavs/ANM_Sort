bf320acbaaa3a724189bcc97158fbb11e02aebd0
Lucas Bueno <53531008+LucasBusavs@users.noreply.github.com>
2023-05-04 10:57:20 -0300

    main()

--- ANM_Sort.py
+++ ANM_Sort.py
@@ -4,6 +4,21 @@ from tkinter import filedialog
 import os
 from zipfile import ZipFile
 
+def fileDialog():
+    filetypes = (
+        ('Google Earth (.kml)', '*.KML'),
+        ('Google Earth (.kmz)', '*.KMZ'),
+    )
+    
+    root = tk.Tk()
+    filePath = tk.filedialog.askopenfilename(
+        title='Select a file...',
+        filetypes=filetypes,
+    )
+    root.destroy()
+    
+    return filePath
+
 def kmzConverter(filePath):
     fileName = filePath.split(".kmz")
     zip = fileName[0] + ".zip"
@@ -20,7 +35,7 @@ def kmzConverter(filePath):
     print('KMZ converted successfully')
     return fileName[0] + ".kml"
 
-def boundPhase(block):
+def boundPhase(block, notFound):
     notFound = []
     start = block.find("<td>Fase</td>")
     if start!=-1:
@@ -33,67 +48,59 @@ def boundPhase(block):
         notFound.append(block)
     return start, end
 
-def phasesName(splitFile):
+def phasesName(splitFile, notFound, phases):
     for block in splitFile:
         if block!=splitFile[0]:
-            start, end = boundPhase(block)
+            start, end = boundPhase(block, notFound)
             try:
                 phases.index(block[start:end])
             except:
                 phases.append(block[start:end])
 
-filetypes = (
-    ('Google Earth (.kml)', '*.KML'),
-    ('Google Earth (.kmz)', '*.KMZ'),
-)
-# open-file dialog
-root = tk.Tk()
-filePath = tk.filedialog.askopenfilename(
-    title='Select a file...',
-    filetypes=filetypes,
-)
-root.destroy()
+def main():
+    notFound = []
+    phases = []
+    kml = ''
 
-notFound = []
-phases = []
+    filePath = fileDialog()
+    if filePath != '':
+        if filePath.endswith(".kmz"):
+            filePath = kmzConverter(filePath)
+        
+        with open(filePath, "r", encoding='utf8') as f:
+            lines = f.read()
+        
+        split = lines.split("<Placemark>")
+        
+        phasesName(split, notFound, phases)
+        phases.sort()
+        print(phases)
+        
+        for phase in phases:
+            for block in split:
+                if block!=split[0]:
+                    if block == split[-1]:
+                        last = block.split("</Placemark>")
+                        block = last[0] + "</Placemark>"
+                    start, end = boundPhase(block, notFound)
+                    if phase == block[start:end]:
+                        try:
+                            kml.index(f'\t\t<Folder>\n\t\t\t<name>{phase}</name>\n')
+                        except:
+                            kml = kml + (f'\t\t<Folder>\n\t\t\t<name>{phase}</name>\n')
+                        kml = kml + '<Placemark>'
+                        kml = kml + block
+            kml = kml + '</Folder>\n'
+        kml = split[0] + kml + last[1]
+        
+        with open(filePath, "w", encoding='utf8') as f:
+            f.write(kml) 
+        
+        if len(notFound) != 0:
+            print("Some not found")
 
-kml = ''
+    else:
+        print("No file selected")
 
-if filePath != '':
-    if filePath.endswith(".kmz"):
-        filePath = kmzConverter(filePath)
-    
-    with open(filePath, "r", encoding='utf8') as f:
-        lines = f.read()
-    
-    split = lines.split("<Placemark>")
-    
-    phasesName(split)
-    
-    print(phases)
-    
-    for phase in phases:
-        for block in split:
-            if block!=split[0]:
-                if block == split[-1]:
-                    last = block.split("</Placemark>")
-                    block = last[0] + "</Placemark>"
-                start, end = boundPhase(block)
-                if phase == block[start:end]:
-                    try:
-                        kml.index(f'\t\t<Folder>\n\t\t\t<name>{phase}</name>\n')
-                    except:
-                        kml = kml + (f'\t\t<Folder>\n\t\t\t<name>{phase}</name>\n')
-                    kml = kml + '<Placemark>'
-                    kml = kml + block
-        kml = kml + '</Folder>\n'
-    kml = split[0] + kml + last[1]
-    
-    with open(filePath, "w", encoding='utf8') as f:
-        f.write(kml) 
-    
-    if len(notFound) != 0:
-        print("Some not found")
-
-else:
-    print("No file selected")
+if __name__ == '__main__':
+    main()
