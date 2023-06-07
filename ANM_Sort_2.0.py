d9350a43d1db3a8ea970794c89942e0dbd33c4b5
Lucas Bueno <53531008+LucasBusavs@users.noreply.github.com>
2023-06-06 21:56:47 -0300

    Line by line

---
+++ ANM_Sort_2.0.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+# Algorithm to organize Brazilian mining data in folders, by their phases
+
+import tkinter as tk
+from tkinter import filedialog
+import os
+from zipfile import ZipFile
+import time
+import keyboard
+from tqdm import tqdm
+
+'''
+Function to generate a file dialog to select the right file to be open
+Returns a string containing the selected file path
+'''
+def fileDialog():
+    filetypes = (
+        ('Google Earth (.kmz)', '*.KMZ'),
+        ('Google Earth (.kml)', '*.KML'),
+    )
+    
+    root = tk.Tk()
+    filePath = tk.filedialog.askopenfilename(
+        title='Select a file...',
+        filetypes=filetypes,
+    )
+    root.destroy()
+    
+    return filePath
+
+'''
+Function to convert a .kmz file into a .kml file
+Receive the path of.kmz file
+Returns the new path of the .kml file
+'''
+def kmzConverter(filePath):
+    fileName = filePath.split(".kmz")
+    zip = fileName[0] + ".zip"
+    os.rename(filePath, zip)
+    path = os.path.dirname(zip)
+    dir1 = os.listdir(path)
+    with ZipFile(zip, "r") as z:
+        z.extractall(path)
+    dir2 = os.listdir(path)
+    kmlFile = [i for i in dir2 if i not in dir1]
+    kmlFile = kmlFile[0]
+    os.remove(zip)
+    os.rename(path + "/" + kmlFile, fileName[0] + ".kml")
+    print('KMZ converted successfully')
+    return fileName[0] + ".kml"
+
+def main():
+    dictIndex = {}
+    j = 0
+    start = 0
+    end = 0
+    value = ''
+
+    filePath = fileDialog()
+    begin = time.time()
+    if filePath != '':
+        if filePath.endswith(".kmz"):
+            filePath = kmzConverter(filePath)
+        
+        with open(filePath, "r", encoding='utf8') as f:
+            lines = f.readlines()
+            for i in range(len(lines)):
+                if lines[i] == '    <Placemark>\n':
+                    start = i
+                elif lines[i] == '<td>Fase</td>\n':
+                    value = lines[i+2]
+                elif lines[i] == '    </Placemark>\n':
+                    end = i
+                    seTuple = (start, end)
+                    dictIndex[seTuple] = value
+                    print(dictIndex)
+        print(j)
+    else:
+        print("No file selected")
+    return time.time() - begin
+
+if __name__ == '__main__':
+    executeTime = main()
+    print(f'Time to execute in seconds: {executeTime: .5f}')
+    print('Press enter to continue...')
+    keyboard.wait("enter")
