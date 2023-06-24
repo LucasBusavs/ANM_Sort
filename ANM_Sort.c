f42755de9a7cba4b086054c79c6c36fee7e08177
Lucas Bueno <turmadaciencia1@gmail.com>
2023-06-24 01:24:51 -0300

    readLines

--- ANM_Sort.c
+++ ANM_Sort.c
@@ -19,12 +19,61 @@ struct Index{
     char phaseType[38];  //Maximum size of phaseType type
 };
 
-/*
-Description: Function to compare two phases types, in alphabet order
-Parameters: 2 blocks 
-*/
-int compare(struct Index* a, struct Index* b){
-    return strcmp(a->phaseType, b->phaseType);
+void readLines(int* numLines, char*** lines){
+    int i;
+    int linesSize = INITIAL_SIZE;
+    char buffer[1024]; //Maximum length of the line
+
+    *lines = (char **) malloc(linesSize * sizeof(char *));
+    if (!(*lines)) {
+        printf("Error allocating memory.\n");
+        return;
+    }
+    
+    //Opening file for reading
+    FILE *f = fopen("PB.kml", "r");
+    if (!f) {
+        printf("Error opening file.\n");
+        return;
+    }
+
+    //Reading file line by line
+    while (fgets(buffer, sizeof(buffer), f) != NULL) {
+        //Allocate memory for lines
+        (*lines)[(*numLines)] = (char *) malloc((strlen(buffer) + 1) * sizeof(char));
+        if (!(*lines)[(*numLines)]) {
+            printf("Error allocating memory.\n");
+            fclose(f);
+            //Free previously allocated memory
+            for (i = 0; i < *numLines; i++) {
+                free(lines[i]);
+            }
+            free(lines);
+            return;
+        }
+
+        //Coping line into the array of lines
+        strcpy((*lines)[(*numLines)], buffer);
+        (*numLines)++;
+
+        //Check if lines needs to be reallocated
+        if (*numLines >= linesSize) {
+            linesSize += INCREMENT_SIZE;
+            *lines = (char **) realloc(*lines, linesSize * sizeof(char *));
+            if (!(*lines)) {
+                printf("Error reallocating memory.\n");
+                fclose(f);
+                //Free previously allocated memory
+                for (i = 0; i < (*numLines); i++) {
+                    free(lines[i]);
+                }
+                free(lines);
+                return;
+            }
+        }
+    }
+    fclose(f);
+    //Finished reading the file
 }
 
 void indexing(struct Index** listIndex, int numLines, int* qntProcess, char** lines, int* firstLine, int* lastLine){
@@ -37,7 +86,7 @@ void indexing(struct Index** listIndex, int numLines, int* qntProcess, char** li
         printf("Error allocating memory.\n");
         return;
     }
-    
+
     //Generate the listIndex of all mining processes
     for (int i = 0; i < numLines; i++) {
         if(strcmp(lines[i], "    <Placemark>\n") == 0){
@@ -67,6 +116,14 @@ void indexing(struct Index** listIndex, int numLines, int* qntProcess, char** li
             }
         }
     }
+}
+
+/*
+Description: Function to compare two phases types, in alphabet order
+Parameters: 2 blocks 
+*/
+int compare(struct Index* a, struct Index* b){
+    return strcmp(a->phaseType, b->phaseType);
 }
 
 void phaseFrequency(int** qnt, struct Index* listIndex, int qntProcess) {
@@ -135,110 +192,15 @@ void write(int firstLine, int lastLine, int qntProcess, int numLines, struct Ind
 
 int main() {
     int lastLine = 0, firstLine = 0;
-    int flag = 0;
-    int j = 4, i;
     int qntProcess = 0;
-    int sizeIndex = 1;
-    int linesSize = INITIAL_SIZE;
     int numLines = 0;
-    char buffer[1024]; //Maximum length of the line
     int* qnt;
+    char** lines;
     struct Index* listIndex;
 
-    //********Alocating memory**********
-    //Memory allocation for a matrix containing the file's lines, line by line
-    char** lines = (char **) malloc(linesSize * sizeof(char *));
-    if (!lines) {
-        printf("Error allocating memory.\n");
-        return 1;
-    }
-    
-    /*
-    struct Index* listIndex = (struct Index*) malloc(sizeIndex * sizeof(struct Index)); 
-    if (!listIndex) {
-        printf("Error allocating memory.\n");
-        return 1;
-    }
-    */
-
-    //Opening file for reading
-    FILE *f = fopen("PB.kml", "r");
-    if (!f) {
-        printf("Error opening file.\n");
-        return 1;
-    }
-
-    //Reading file line by line
-    while (fgets(buffer, sizeof(buffer), f) != NULL) {
-        //Allocate memory for lines
-        lines[numLines] = (char *) malloc((strlen(buffer) + 1) * sizeof(char));
-        if (!lines[numLines]) {
-            printf("Error allocating memory.\n");
-            fclose(f);
-            //Free previously allocated memory
-            for (i = 0; i < numLines; i++) {
-                free(lines[i]);
-            }
-            free(lines);
-            return 1;
-        }
-
-        //Coping line into the array of lines
-        strcpy(lines[numLines], buffer);
-        numLines++;
-
-        //Check if lines needs to be reallocated
-        if (numLines >= linesSize) {
-            linesSize += INCREMENT_SIZE;
-            lines = (char **) realloc(lines, linesSize * sizeof(char *));
-            if (!lines) {
-                printf("Error reallocating memory.\n");
-                fclose(f);
-                //Free previously allocated memory
-                for (int i = 0; i < numLines; i++) {
-                    free(lines[i]);
-                }
-                free(lines);
-                return 1;
-            }
-        }
-    }
-    fclose(f);
-    //Finished reading the file
+    readLines(&numLines, &lines);
     
     indexing(&listIndex, numLines, &qntProcess, lines, &firstLine, &lastLine);
-
-    /*
-    //Generate the listIndex of all mining processes
-    for (i = 0; i < numLines; i++) {
-        if(strcmp(lines[i], "    <Placemark>\n") == 0){
-            qntProcess++;
-            if(!flag){
-                firstLine = i;
-                flag = 1;
-            }
-            listIndex[sizeIndex - 1].start = i;
-        }
-        else if(strcmp(lines[i], "<td>Fase</td>\r\n") == 0){
-            while(lines[i+1][j] != '<'){
-                listIndex[sizeIndex - 1].phaseType[j-4] = lines[i+1][j];
-                j++;
-            }
-            listIndex[sizeIndex - 1].phaseType[j-4] = '\0';
-            j = 4;
-        }
-        else if(strcmp(lines[i], "    </Placemark>\n") == 0){
-            lastLine = i + 1;
-            listIndex[sizeIndex - 1].end = i;
-            listIndex = realloc(listIndex, ++sizeIndex * sizeof(struct Index));
-            if(!listIndex){
-                printf("Error reallocating memory.\n");
-                free(listIndex);
-                return 1;
-            }
-        }
-    }
-    */
 
     //Quick sorting lisIndex alphabetically, by phaseType
     qsort(listIndex, qntProcess, sizeof(struct Index), compare);
@@ -249,7 +211,7 @@ int main() {
 
     printf("FINISHED");
 
-    for (i = 0; i < numLines; i++) {
+    for (int i = 0; i < numLines; i++) {
         free(lines[i]);
     }
     free(lines);
